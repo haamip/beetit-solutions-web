101ce21f33131c79f9c67d3415a585adb1a4dd31
haamip <81553874+haamip@users.noreply.github.com>
2026-09-08 17:59:18 +1200

    Load complete admin styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './booking-admin.css'
+import './admin.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
